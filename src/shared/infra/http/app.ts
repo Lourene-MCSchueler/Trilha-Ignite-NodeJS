601966fc258b4b4301fae9ff50b2328d246776f4
Lourene Schueler <lourenecamargo@hotmail.com>
2022-09-12 14:41:05 -0300

    cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
-import "dotenv/config"
+import "dotenv/config";
+import cors from "cors";
 import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
@@ -7,11 +8,10 @@ import swaggerUi from "swagger-ui-express";
 import { AppError } from "@shared/errors/AppError";
 import createConnection from "@shared/infra/typeorm";
 
+import upload from "../../../config/upload";
 import swaggerFile from "../../../swagger.json";
 import { router } from "./routes";
-
 import "@shared/container";
-import upload from "../../../config/upload";
 
 createConnection();
 const app = express();
@@ -20,9 +20,10 @@ app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
-app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`))
-app.use("/cars", express.static(`${upload.tmpFolder}/cars`))
+app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
+app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
+app.use(cors());
 app.use(router);
 
 app.use(
